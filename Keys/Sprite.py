b2b9314b0929e9953f221296d5aebd249b05af5b
Sherman-Firefly <efracw2007@gmail.com>
2024-12-12 18:07:00 +0800

    update

--- Keys/Sprite.py
+++ Keys/Sprite.py
@@ -3,37 +3,47 @@ import pygame
 pygame.init()
 screen=pygame.display.set_mode((500,500))
 screen.fill((255,255,255))
-colors={'Blue': pygame.color('Blue'),'Red': pygame.color('Red'), 'Yellow': pygame.color('Yellow') }
-done=False
-is_blue=True
 
-x=60
-y=60
+colors={'Blue': pygame.Color('Blue'),'Red': pygame.Color('Red'), 'Yellow': pygame.Color('Yellow') }
+done=False
+color_key= 'Blue'
+
+x, y = 60, 60
+sprite_width, sprite_height = 60, 60
+screen_width, screen_height = 500, 500
 
 while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done=True
         if event.type==pygame.KEYDOWN or event.type==pygame.K_SPACE:
-            is_blue= not is_blue
+            if event.key == pygame.K_SPACE:  
+                if color_key == 'Blue':
+                    color_key = 'Red'
+                elif color_key == 'Red':
+                    color_key = 'Yellow'
+                else:
+                    color_key = 'Blue'
 
     pressed=pygame.key.get_pressed()
-    if pressed[pygame.K_UP]:y+=3
-    if pressed[pygame.K_DOWN]:y-=3
+    if pressed[pygame.K_UP]:y-=3
+    if pressed[pygame.K_DOWN]:y+=3
     if pressed[pygame.K_LEFT]:x-=3
-    if pressed[pygame.K_RIGHT]:x-=3
+    if pressed[pygame.K_RIGHT]:x+=3
 
     x= min(max(0,x), screen_width-sprite_width)
     y= min(max(0,y), screen_height-sprite_height)
     
     
+    screen.fill((255, 255, 255))
 
-    if is_blue:
-        color=(0,128,225)
-    else:
-        color=(255,255,255)
-    pygame.draw.rect(screen,color, pygame.Rect(x,y,60,60))
-pygame.display.flip()
+    
+    color = colors[color_key]
 
+    
+    pygame.draw.rect(screen, color, pygame.Rect(x, y, sprite_width, sprite_height))
+
+  
+    pygame.display.flip()
 
 
